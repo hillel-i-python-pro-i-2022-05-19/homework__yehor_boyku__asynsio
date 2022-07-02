f9fa96200d3e974f700e35a9b687d37343d18fe7
yehor <yehorboiku@gmail.com>
2022-07-02 14:15:07 +0300

    lesson 9_fuzz_generator

--- fuzz_generator_sync.py
+++ fuzz_generator_sync.py
@@ -1,32 +1,43 @@
 import concurrent.futures
+import logging
 from typing import Iterator
 
 from settings import CHARACTERS
 from init_logging import init_logging
-from test import generate, DEFAULT_ALPHABET
+from fuzz_generator import generate, DEFAULT_ALPHABET
 
 
 def generator_word(length: int = 5, quantity: int = len(CHARACTERS) + 1, conditions: str = CHARACTERS):
-    generate(word_length=length, quantity=quantity, alphabet=conditions)
-    print(generate)
+    logging.info(generate(word_length=length, quantity=quantity, alphabet=conditions))
+    return generate
 
 
-def calculate_process(quantity) -> Iterator[None]:
-    with concurrent.futures.ProcessPoolExecutor() as worker:
-        results = worker.map(
-            generator_word,
-            quantity,
-        )
-        worker.submit(generator_word, args=(quantity[0],))
-    return results
+def calculate_thread(quantity: list[int]) -> list[str]:
+    with concurrent.futures.ThreadPoolExecutor() as worker:
+        futures = [
+            worker.submit(generator_word, number) for number in quantity
+        ]
+        items = concurrent.futures.as_completed(futures)
+    return items
+
+
+# def calculate_process(quantity: list[int]) -> list[str]:
+#     with concurrent.futures.ProcessPoolExecutor() as worker:
+#         results = worker.map(
+#             generator_word,
+#             quantity,
+#         )
+#         worker.submit(generator_word, args=(quantity[0],))
+#     return results
 
 
 def main():
     alphabet = DEFAULT_ALPHABET
     word_length = 5
     quantity = len(CHARACTERS)
-    combinations = list(range(quantity, quantity + 10))
-    calculate_process(quantity=combinations)
+    combinations = list(range(1, quantity + 1))
+    # calculate_process(quantity=combinations)
+    calculate_thread(quantity=combinations)
     print()
 
 
